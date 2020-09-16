785b06ab83381a8247beac2c9c719f569d04c8da
Ed Baskerville <ed@edbaskerville.com>
2020-09-16 09:56:48 -0700

    Modified meta-seed stuff. Added immigration rate sweep

--- example-sweep/example-sweep.py
+++ example-sweep/example-sweep.py
@@ -6,31 +6,32 @@ import json
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 JULIA_SCRIPT_PATH = os.path.abspath(
-    os.path.join(SCRIPT_DIR, '..', 'crispr-sim', 'julia', 'main.jl')
+    os.path.join(SCRIPT_DIR, '..', 'julia', 'main.jl')
 )
 
 RUNS_PATH = os.path.join(SCRIPT_DIR, 'runs')
 
-N_REPLICATES = 35
+N_REPLICATES = 1
 
-# This is the seed that generates seeds for each simulation. This is to compare simulations with and without immigration. This is not part of original code per Ed Baskerville
-metaseed = 111
-random.seed(metaseed)
-seedList = [random.randrange(1, 2**63 - 1) for i in range(N_REPLICATES)]
+# Seed for seed generator, for reproducible sweeps
+# If set to None, then the random.SystemRandom() device will be used to generate
+# seeds.
+# Otherwise, random.Random(META_SEED) will be used.
+# WARNING: both Python and Julia use Mersenne Twister, and it's statistically
+# dangerous to use the same RNG to generate seeds, so setting META_SEED could
+# create some weird correlations.
+# (But unlikely to make a real difference in simulation analyses, I suspect.)
+META_SEED = None
 
-RUN_PARAMETERS = {
+PARAMETERS = {
     "t_final" : 5000.0,
-    "t_output" : 1.0
-}
-
-INITIALIZATION_PARAMETERS = {
+    "t_output" : 1.0,
+    
     "n_bstrains" : 1,
     "n_hosts_per_bstrain" : 100,
     "n_vstrains" : 1,
-    "n_particles_per_vstrain" : 100
-}
-
-MODEL_PARAMETERS = {
+    "n_particles_per_vstrain" : 100,
+    
     "n_protospacers" : 15,
     "u_n_spacers_max" : 10,
     "p_crispr_failure_prob" : 1e-5,
@@ -46,12 +47,8 @@ MODEL_PARAMETERS = {
     
     "d_death_rate" : 1e-18,
     
-    #"decouple_viral_mutation" : false,
-    
     "g_immigration_rate" : 1e-18
 }
-
-
 
 
 SBATCH_TEMPLATE = \
@@ -84,12 +81,19 @@ def main():
     # Create root output directory
     os.makedirs(RUNS_PATH)
     
+    # Seed RNG
+    if META_SEED is None:
+        seed_rng = random.SystemRandom()
+    else:
+        seed_rng = random.Random(META_SEED)
+    
     # Set up all directories
     run_paths = []
     for n_protospacers in [15]:
         for u_n_spacers_max in [10]:
-            for run_path in set_up_replicates(n_protospacers, u_n_spacers_max):
-                run_paths.append(run_path)
+            for g_X_1e18 in [0, 1, 2, 4]:
+                for run_path in set_up_replicates(seed_rng, n_protospacers, u_n_spacers_max, g_X_1e18):
+                    run_paths.append(run_path)
     
     # Create a shell script to submit everything
     with open(os.path.join(SCRIPT_DIR, 'submit.sh'), 'w') as f:
@@ -99,28 +103,27 @@ def main():
             f.write('cd {}\n'.format(run_path))
             f.write('sbatch run.sbatch\n')
 
-def set_up_replicates(n_protospacers, u_n_spacers_max):
-    # Set up model parameters for these replicates
-    model_parameters = {
-        **MODEL_PARAMETERS,
-        "n_protospacers" : n_protospacers,
-        "u_n_spacers_max" : u_n_spacers_max
-    }
+def set_up_replicates(seed_rng, n_protospacers, u_n_spacers_max, g_immigration_rate_X_1e18):
+    g_immigration_rate = g_immigration_rate_X_1e18 * 1e-18
     
     # Create a parameters file for each replicate
     for i in range(N_REPLICATES):
-        # Set up run parameters (random seed) for each replicate
-        run_parameters = {
-            **RUN_PARAMETERS,
-            "rng_seed" : seedList[i]
+        # Set up parameters for this replicate
+        parameters = {
+            **PARAMETERS,
+            "n_protospacers" : n_protospacers,
+            "u_n_spacers_max" : u_n_spacers_max,
+            "g_immigration_rate" : g_immigration_rate,
+            "rng_seed" : seed_rng.randrange(1, 2**31 - 1)
         }
         
         # Create subdirectory for replicate
         run_path = os.path.join(
             RUNS_PATH,
-            'nps={0}-u={1}'.format(
+            'nps={0}-u={1}-g={2}'.format(
                 n_protospacers,
-                u_n_spacers_max
+                u_n_spacers_max,
+                g_immigration_rate_X_1e18
             ),
             '{0}'.format(i)
         )
@@ -129,11 +132,7 @@ def set_up_replicates(n_protospacers, u_n_spacers_max):
         # Save parameters file for replicate
         params_path = os.path.join(run_path, 'parameters.json')
         with open(params_path, 'w') as f:
-            json.dump({
-                **run_parameters,
-                **INITIALIZATION_PARAMETERS,
-                **model_parameters
-            }, f, indent = 4)
+            json.dump(parameters, f, indent = 4)
         
         # Save sbatch file for replicate
         sbatch_path = os.path.join(run_path, 'run.sbatch')
