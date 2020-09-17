b622d12dd22e84fe1a8d9a67dd851dad3aa551b1
Armun <liaghat.armun@gmail.com>
2020-09-16 21:12:49 -0500

    Fixed paths for my intended use. Redirected plot saving to another file of plots in the home directory. Created titles and axes for plots. Titles are based on relative path names.

--- sergioCodes4Analysis/master/PLOTAbundanceStackedPlot.py
+++ sergioCodes4Analysis/master/PLOTAbundanceStackedPlot.py
@@ -55,21 +55,30 @@ def StackedPlotDF(data,tp):
 # ######################################
 
 
-stacked_plot = pd.read_csv('Bacteria_Abundance-StackedPlot-DataFrame.csv', delimiter=',')
-Vstacked_plot = pd.read_csv('Virus_Abundance-StackedPlot-DataFrame.csv', delimiter=',')
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+stacked_plot = pd.read_csv(os.path.join(SCRIPT_DIR, 'Bacteria_Abundance-StackedPlot-DataFrame.csv'), delimiter=',')
+Vstacked_plot = pd.read_csv(os.path.join(SCRIPT_DIR, 'Virus_Abundance-StackedPlot-DataFrame.csv'), delimiter=',')
 
+#this is the relative path of a particular simulation
+sim_dir = os.path.relpath(SCRIPT_DIR,   os.path.join(SCRIPT_DIR,'..','..'));
 
 #plt.figure(figsize=(10, 10), dpi= 80)
 stacked_plot.plot.area(stacked=True, legend=False, linewidth=0);
 #plt.show()
-plt.tight_layout()  
-plt.savefig("Bacteria-Abundance_stacked_plot.png",dpi=500)
-plt.close()   
+plt.title('Bacterial Abundances: ' + sim_dir )
+plt.xlabel('Time t')
+plt.ylabel('Abundances N_i')
+plt.tight_layout()
+plt.savefig(os.path.join(SCRIPT_DIR,'..','..','..','plots',sim_dir,'Bacteria-Abundance_stacked_plot.png'),dpi=500)
+plt.close()
 
 
 #plt.figure(figsize=(10, 10), dpi= 80)
 Vstacked_plot.plot.area(stacked=True, legend=False, linewidth=0);
 #plt.show()
-plt.tight_layout()  
-plt.savefig("Virus-Abundance_stacked_plot.png",dpi=500)
+plt.title('Viral Abundances: ' + sim_dir )
+plt.xlabel('Time t')
+plt.ylabel('Abundances V_i')
+plt.tight_layout()
+plt.savefig(os.path.join(SCRIPT_DIR,'..','..','..','plots',sim_dir,'Virus-Abundance_stacked_plot.png'),dpi=500)
 plt.close()   
