0e246bdf796f4b556d562370a9c092fcded6609f
Armun <liaghat.armun@gmail.com>
2020-09-16 21:31:17 -0500

    Added stacked-data-frame script and make-stacked-plot scripts

--- example-sweep/example-sweep.py
+++ example-sweep/example-sweep.py
@@ -66,6 +66,126 @@ module load julia
 
 '''
 
+
+STACKED_DF = \
+'''#!/usr/bin/env python3
+
+    import pandas as pd
+    import numpy as np
+    import scipy as sp
+    import math
+    import matplotlib.pyplot as plt
+    import sys
+    import os
+    # import mpld3
+
+
+    import seaborn as sns
+    from scipy import stats
+
+
+
+    def StackedPlotDF(data,tp):
+        
+        stacked_plot = pd.DataFrame()
+        ID = "bact"
+
+        if (tp=="bact"):
+            ID = "bstrain_id"
+        elif (tp=="vir"):
+            ID = "vstrain_id"
+        
+        
+        #strains = data['bstrain_id'].unique()
+        strains = data[ID].unique()
+        tl = len(data["t"].unique())
+        
+        stacked_plot["t"] = data["t"].unique()
+
+        for s in strains:
+
+            Abs = np.zeros(tl)
+            
+            #tmp = data[data["bstrain_id"]==s]
+            tmp = data[data[ID]==s]
+            
+            for i in tmp["t"].values:
+                abun = tmp[tmp["t"]==i]["abundance"].values
+                Abs[int(i)] = abun
+            
+            stacked_plot[s] = Abs
+            
+        
+        return stacked_plot
+
+
+
+
+    # ######################################
+
+    SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+    bact = pd.read_csv(os.path.join(SCRIPT_DIR, 'babundance.csv'), delimiter=',')
+    phage = pd.read_csv(os.path.join(SCRIPT_DIR, 'vabundance.csv'), delimiter=',')
+    #data = pd.read_csv(path+'time-series-data.txt', delimiter=' ')
+
+
+
+    stacked_plot = StackedPlotDF(bact,"bact")
+    Vstacked_plot = StackedPlotDF(phage,"vir")
+
+    stacked_plot.to_csv(r'Bacteria_Abundance-StackedPlot-DataFrame.csv')
+    Vstacked_plot.to_csv(r'Virus_Abundance-StackedPlot-DataFrame.csv')
+'''
+
+STACKED_PLOTS = \
+'''
+
+    import pandas as pd
+    import numpy as np
+    import scipy as sp
+    import math
+    import matplotlib.pyplot as plt
+    import sys
+    import os
+    # import mpld3
+
+    import seaborn as sns
+    from scipy import stats
+
+
+    SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+    stacked_plot = pd.read_csv(os.path.join(SCRIPT_DIR, 'Bacteria_Abundance-StackedPlot-DataFrame.csv'), delimiter=',')
+    Vstacked_plot = pd.read_csv(os.path.join(SCRIPT_DIR, 'Virus_Abundance-StackedPlot-DataFrame.csv'), delimiter=',')
+
+    #this is the relative path of a particular simulation
+    sim_dir = os.path.relpath(SCRIPT_DIR,   os.path.join(SCRIPT_DIR,'..','..'));
+
+    #plt.figure(figsize=(10, 10), dpi= 80)
+    stacked_plot.plot.area(stacked=True, legend=False, linewidth=0);
+    #plt.show()
+    plt.title('Bacterial Abundances: ' + sim_dir )
+    plt.xlabel('Time t')
+    plt.ylabel('Abundances N_i')
+    plt.tight_layout()
+    plt.savefig(os.path.join(SCRIPT_DIR,'..','..','..','plots',sim_dir,'Bacteria-Abundance_stacked_plot.png'),dpi=500)
+    plt.close()
+
+
+    #plt.figure(figsize=(10, 10), dpi= 80)
+    Vstacked_plot.plot.area(stacked=True, legend=False, linewidth=0);
+    #plt.show()
+    plt.title('Viral Abundances: ' + sim_dir )
+    plt.xlabel('Time t')
+    plt.ylabel('Abundances V_i')
+    plt.tight_layout()
+    plt.savefig(os.path.join(SCRIPT_DIR,'..','..','..','plots',sim_dir,'Virus-Abundance_stacked_plot.png'),dpi=500)
+    plt.close()
+    
+'''
+
+
+
 def main():
     # Create root output directory
     os.makedirs(RUNS_PATH)
