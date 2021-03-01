dabc783812853b208e8edb81884b1c127f208baf
Armun <liaghat.armun@gmail.com>
2021-03-01 10:20:39 -0600

    Decimal print of UInt32.

--- julia/main.jl
+++ julia/main.jl
@@ -10,8 +10,9 @@ using Logging
 #     )
 # )
 
-# Ensure that UInt64 gets printed in decimal, not hex
+# Ensure that UInt64 and UInt32 gets printed in decimal, not hex
 Base.show(io::IO, x::UInt64) = Base.print(io, x)
+Base.show(io::IO, y::UInt32) = Base.print(io, y)
 
 include("model.jl")
 
