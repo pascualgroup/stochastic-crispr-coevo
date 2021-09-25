6a5730ca8b01786f90d53d61b565d11905c241e5
rliag11 <liaghat.armun@gmail.com>
2021-09-25 14:01:08 -0500

    Removed one of the conditionals, was not acting as bug but made data incomplete. Also added param_combos and runs info to shannon.sqlite.

--- data-analysis/shannon/shannon.jl
+++ data-analysis/shannon/shannon.jl
@@ -7,11 +7,10 @@ using DataFrames
 using SQLite.DBInterface: execute
 
 
-run_id = ARGS[1] # cluster & local
-#run_id = 1 # local
+run_id = ARGS[1]
 
 ## Define Paths ##
-SCRIPT_PATH = abspath(dirname(PROGRAM_FILE)) # cluster
+SCRIPT_PATH = abspath(dirname(PROGRAM_FILE))
 
 dbSimPath = joinpath(SCRIPT_PATH,"..","..","simulation","sweep_db_gathered.sqlite") # cluster
 #dbSimPath = joinpath("/Volumes/Yadgah/sweep_db_gathered.sqlite") # local
@@ -19,8 +18,8 @@ dbSimPath = joinpath(SCRIPT_PATH,"..","..","simulation","sweep_db_gathered.sqlit
 dbSimInfoPath = joinpath(SCRIPT_PATH,"..","..","simulation","sweep_db.sqlite") # cluster
 #dbSimInfoPath = joinpath("/Volumes/Yadgah/sweep_db.sqlite") # local
 
-if isfile("shannon_output.sqlite") # cluster
-    error("shannon_output.sqlite already exists; delete first") # cluster
+if isfile("shannon_output.sqlite")
+    error("shannon_output.sqlite already exists; delete first")
 end # cluster
 dbOutput = SQLite.DB("shannon_output.sqlite") # cluster
 #dbOutput = SQLite.DB("/Volumes/Yadgah/shannon_output.sqlite") # local
@@ -65,7 +64,32 @@ function shannon(dbTemp,dbOutput)
         #IT ONLY SAVES AS A NUMBER WHEN A LOOP ARGUMENT
         btotal = totAbunds.microbial_abundance
         vtotal = totAbunds.viral_abundance
-        #println(bshannon)
+
+        bsubAbunds = [bsubAbunds.abundance for bsubAbunds in execute(dbTemp, "SELECT abundance FROM babundance WHERE t = ?", (time,))]
+        brelAbunds = bsubAbunds./btotal
+        brelAbunds = brelAbunds[brelAbunds.>=1e-200]
+
+        # This is assuming diversity will not reach +Inf
+        #if true in isnan.(brelAbunds.*(log.(brelAbunds)))
+            #bhill2 = 0
+        #else
+            bhill2 = exp(-1*sum(brelAbunds.*(log.(brelAbunds))))
+        #end
+
+        vsubAbunds = [vsubAbunds.abundance for vsubAbunds in execute(dbTemp, "SELECT abundance FROM vabundance WHERE t = ?", (time,))]
+        vrelAbunds = vsubAbunds./vtotal
+        vrelAbunds = vrelAbunds[vrelAbunds.>=1e-200]
+
+        # This is assuming diversity will not reach +Inf
+        #if true in isnan.(vrelAbunds.*(log.(vrelAbunds)))
+            #vhill2 = 0
+        #else
+            vhill2 = exp(-1*sum(vrelAbunds.*(log.(vrelAbunds))))
+        #end
+
+        execute(dbOutput, "BEGIN TRANSACTION")
+        execute(dbOutput, "INSERT INTO hill_no2 VALUES (?,?,?)", (time,vhill2,bhill2))
+        execute(dbOutput, "COMMIT")
 
         if vtotal == 0 && btotal >= CARRYING_CAP
             println("Virus has gone extinct! Also, microbes are at carrying capacity. Truncating data and projecting rest of computation...")
@@ -76,46 +100,38 @@ function shannon(dbTemp,dbOutput)
 
             bsubAbunds = [bsubAbunds.abundance for bsubAbunds in execute(dbTemp, "SELECT abundance FROM babundance WHERE t = ?", (time,))]
             brelAbunds = bsubAbunds./btotal
-            brelAbunds = brelAbunds[brelAbunds.>=1e-64]
-            bhill2 = exp(-1*sum(brelAbunds.*(log.(brelAbunds))))
+            brelAbunds = brelAbunds[brelAbunds.>=1e-200]
+
+            # This is assuming diversity will not reach +Inf
+            #if true in isnan.(brelAbunds.*(log.(brelAbunds)))
+                #bhill2 = 0
+            #else
+                bhill2 = exp(-1*sum(brelAbunds.*(log.(brelAbunds))))
+            #end
+
 
             tempDF = DataFrame(t = Array{Float64,1}(timeDataTrunc),vhill2 = Array{Float64,1}(zeros(timesLeft)),bhill2 = Array{Float64,1}(bhill2*ones(timesLeft)))
             tempDF |> SQLite.load!(dbOutput,"hill_no2",ifnotexists=true)
 
             return
         end
-
-        if vtotal == 0 && btotal < CARRYING_CAP
-            vhill2 = 0
-
-            bsubAbunds = [bsubAbunds.abundance for bsubAbunds in execute(dbTemp, "SELECT abundance FROM babundance WHERE t = ?", (time,))]
-            brelAbunds = bsubAbunds./btotal
-            brelAbunds = brelAbunds[brelAbunds.>=1e-64]
-            bhill2 = exp(-1*sum(brelAbunds.*(log.(brelAbunds))))
-
-            execute(dbOutput, "BEGIN TRANSACTION")
-            execute(dbOutput, "INSERT INTO hill_no2 VALUES (?,?,?)", (time,vhill2,bhill2))
-            execute(dbOutput, "COMMIT")
-        end
-
-        if vtotal != 0
-            bsubAbunds = [bsubAbunds.abundance for bsubAbunds in execute(dbTemp, "SELECT abundance FROM babundance WHERE t = ?", (time,))]
-            brelAbunds = bsubAbunds./btotal
-            brelAbunds = brelAbunds[brelAbunds.>=1e-64]
-            bhill2 = exp(-1*sum(brelAbunds.*(log.(brelAbunds))))
-
-            vsubAbunds = [vsubAbunds.abundance for vsubAbunds in execute(dbTemp, "SELECT abundance FROM vabundance WHERE t = ?", (time,))]
-            vrelAbunds = vsubAbunds./vtotal
-            vrelAbunds = vrelAbunds[vrelAbunds.>=1e-64]
-            vhill2 = exp(-1*sum(vrelAbunds.*(log.(vrelAbunds))))
-
-            execute(dbOutput, "BEGIN TRANSACTION")
-            execute(dbOutput, "INSERT INTO hill_no2 VALUES (?,?,?)", (time,vhill2,bhill2))
-            execute(dbOutput, "COMMIT")
-        end
     end
 end
 
 shannon(dbTemp,dbOutput)
+
+tableNamesTypes = ["$(table_info.name) $(table_info.type)" for table_info in execute(dbSimInfo,"PRAGMA table_info(param_combos)")]
+tableNamesTypes = join(tableNamesTypes,", ")
+
+execute(dbOutput, "CREATE TABLE runs (run_id INTEGER, combo_id INTEGER, replicate INTEGER)")
+execute(dbOutput, "CREATE TABLE param_combos ($(tableNamesTypes...))")
+
+tableNames = ["$(table_info.name)" for table_info in execute(dbSimInfo,"PRAGMA table_info(param_combos)")]
+tableNames = join(tableNames,", ")
+execute(dbOutput, "BEGIN TRANSACTION")
+execute(dbOutput,"ATTACH DATABASE '$(dbSimInfoPath)' as dbSimInfo")
+execute(dbOutput,"INSERT INTO param_combos($(tableNames)) SELECT * FROM dbSimInfo.param_combos")
+execute(dbOutput,"INSERT INTO runs (run_id, combo_id, replicate) SELECT run_id, combo_id, replicate FROM dbSimInfo.runs")
+execute(dbOutput, "COMMIT")
 
 println("Complete!")
