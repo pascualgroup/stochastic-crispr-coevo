1c0ff4e06e889ccb96bb493ba9c2e7458a0d3cd5
rliag11 <liaghat.armun@gmail.com>
2021-10-19 14:32:26 -0500

    Added tables of run_ids, combo_id and paramerter combinations.

--- data-analysis/gather-analysis-output.jl
+++ data-analysis/gather-analysis-output.jl
@@ -82,6 +82,23 @@ function main()
 
         execute(db, "COMMIT")
     end
+
+    dbOutput = SQLite.DB(joinpath(an_dir,"$(analysisType).sqlite"))
+    dbSimInfoPath = joinpath(SCRIPT_PATH,"..","simulation","sweep_db.sqlite") # cluster
+    #run(`cd`) # local
+    #dbSimInfoPath = joinpath("/Volumes/Yadgah/sweep_db.sqlite") # local
+    dbSimInfo = SQLite.DB(dbSimInfoPath)
+    tableNamesTypes = ["$(table_info.name) $(table_info.type)" for table_info in execute(dbSimInfo,"PRAGMA table_info(param_combos)")]
+    tableNamesTypes = join(tableNamesTypes,", ")
+    execute(dbOutput, "CREATE TABLE runs (run_id INTEGER, combo_id INTEGER, replicate INTEGER)")
+    execute(dbOutput, "CREATE TABLE param_combos ($(tableNamesTypes...))")
+    tableNames = ["$(table_info.name)" for table_info in execute(dbSimInfo,"PRAGMA table_info(param_combos)")]
+    tableNames = join(tableNames,", ")
+    execute(dbOutput, "BEGIN TRANSACTION")
+    execute(dbOutput,"ATTACH DATABASE '$(dbSimInfoPath)' as dbSimInfo")
+    execute(dbOutput,"INSERT INTO param_combos($(tableNames)) SELECT * FROM dbSimInfo.param_combos")
+    execute(dbOutput,"INSERT INTO runs (run_id, combo_id, replicate) SELECT run_id, combo_id, replicate FROM dbSimInfo.runs")
+    execute(dbOutput, "COMMIT")
 end
 
 main()
