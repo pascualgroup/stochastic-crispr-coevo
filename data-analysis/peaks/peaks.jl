0ada5ed7dbe5418cdb4ddbc13ff5e69894345415
rliag11 <liaghat.armun@gmail.com>
2021-10-20 15:41:28 -0500

    Took out run_id and param combos info. Changed names with hill2 into shannon diversity. Should be hill1 anyway, oops. Also took out truncation/projection of diversity for future uses that don't have similar dynamics.

--- data-analysis/peaks/peaks.jl
+++ data-analysis/peaks/peaks.jl
@@ -96,22 +96,4 @@ peakSeries |> SQLite.load!(dbOutput,"microbial_peak_series",ifnotexists=true)
 peakDurations |> SQLite.load!(dbOutput,"microbial_peak_durations",ifnotexists=true)
 numpeaks |> SQLite.load!(dbOutput,"microbial_num_peaks",ifnotexists=true)
 
-dbSimInfoPath = joinpath(SCRIPT_PATH,"..","..","simulation","sweep_db.sqlite") # cluster
-#run(`cd`) # local
-#dbSimInfoPath = joinpath("/Volumes/Yadgah/sweep_db.sqlite") # local
-dbSimInfo = SQLite.DB(dbSimInfoPath)
-tableNamesTypes = ["$(table_info.name) $(table_info.type)" for table_info in execute(dbSimInfo,"PRAGMA table_info(param_combos)")]
-tableNamesTypes = join(tableNamesTypes,", ")
-
-execute(dbOutput, "CREATE TABLE runs (run_id INTEGER, combo_id INTEGER, replicate INTEGER)")
-execute(dbOutput, "CREATE TABLE param_combos ($(tableNamesTypes...))")
-
-tableNames = ["$(table_info.name)" for table_info in execute(dbSimInfo,"PRAGMA table_info(param_combos)")]
-tableNames = join(tableNames,", ")
-execute(dbOutput, "BEGIN TRANSACTION")
-execute(dbOutput,"ATTACH DATABASE '$(dbSimInfoPath)' as dbSimInfo")
-execute(dbOutput,"INSERT INTO param_combos($(tableNames)) SELECT * FROM dbSimInfo.param_combos")
-execute(dbOutput,"INSERT INTO runs (run_id, combo_id, replicate) SELECT run_id, combo_id, replicate FROM dbSimInfo.runs")
-execute(dbOutput, "COMMIT")
-
 println("Complete!")
--- data-analysis/shannon/shannon.jl
+++ data-analysis/shannon/shannon.jl
@@ -12,17 +12,17 @@ run_id = ARGS[1]
 ## Define Paths ##
 SCRIPT_PATH = abspath(dirname(PROGRAM_FILE))
 
-dbSimPath = joinpath(SCRIPT_PATH,"..","..","simulation","sweep_db_gathered.sqlite") # cluster
-#dbSimPath = joinpath("/Volumes/Yadgah/sweep_db_gathered.sqlite") # local
+#dbSimPath = joinpath(SCRIPT_PATH,"..","..","simulation","sweep_db_gathered.sqlite") # cluster
+dbSimPath = joinpath("/Volumes/Yadgah/sweep_db_gathered.sqlite") # local
 
-dbSimInfoPath = joinpath(SCRIPT_PATH,"..","..","simulation","sweep_db.sqlite") # cluster
-#dbSimInfoPath = joinpath("/Volumes/Yadgah/sweep_db.sqlite") # local
+#dbSimInfoPath = joinpath(SCRIPT_PATH,"..","..","simulation","sweep_db.sqlite") # cluster
+dbSimInfoPath = joinpath("/Volumes/Yadgah/sweep_db.sqlite") # local
 
 if isfile("shannon_output.sqlite")
     error("shannon_output.sqlite already exists; delete first")
 end # cluster
-dbOutput = SQLite.DB("shannon_output.sqlite") # cluster
-#dbOutput = SQLite.DB("/Volumes/Yadgah/shannon_output.sqlite") # local
+#dbOutput = SQLite.DB("shannon_output.sqlite") # cluster
+dbOutput = SQLite.DB("/Volumes/Yadgah/shannon_output.sqlite") # local
 ##
 
 dbSimInfo = SQLite.DB(dbSimInfoPath)
@@ -31,7 +31,7 @@ combo_id = combo_id.combo_id
 (CARRYING_CAP,) = execute(dbSimInfo,"SELECT microbe_carrying_capacity FROM param_combos WHERE combo_id = ?",(combo_id,))
 CARRYING_CAP = CARRYING_CAP.microbe_carrying_capacity
 
-execute(dbOutput, "CREATE TABLE hill_no2 (t REAL, vhill2 REAL, bhill2 REAL)")
+execute(dbOutput, "CREATE TABLE shannon_diversity (t REAL, vshannon REAL, bshannon REAL)")
 
 # Create temporary database that is a copy of the main database at the run_id value of the script's argument
 #dbTemp = SQLite.DB("/Volumes/Yadgah/timeSeries$(run_id).sqlite") # local
@@ -69,69 +69,53 @@ function shannon(dbTemp,dbOutput)
         brelAbunds = bsubAbunds./btotal
         brelAbunds = brelAbunds[brelAbunds.>=1e-200]
 
-        # This is assuming diversity will not reach +Inf
-        #if true in isnan.(brelAbunds.*(log.(brelAbunds)))
-            #bhill2 = 0
-        #else
-            bhill2 = exp(-1*sum(brelAbunds.*(log.(brelAbunds))))
-        #end
+        if btotal == 0
+            bshannon = 0
+        else
+            bshannon = exp(-1*sum(brelAbunds.*(log.(brelAbunds))))
+        end
 
         vsubAbunds = [vsubAbunds.abundance for vsubAbunds in execute(dbTemp, "SELECT abundance FROM vabundance WHERE t = ?", (time,))]
         vrelAbunds = vsubAbunds./vtotal
         vrelAbunds = vrelAbunds[vrelAbunds.>=1e-200]
 
-        # This is assuming diversity will not reach +Inf
-        #if true in isnan.(vrelAbunds.*(log.(vrelAbunds)))
-            #vhill2 = 0
-        #else
-            vhill2 = exp(-1*sum(vrelAbunds.*(log.(vrelAbunds))))
-        #end
+        if vtotal == 0
+            vshannon = 0
+        else
+            vshannon = exp(-1*sum(vrelAbunds.*(log.(vrelAbunds))))
+        end
 
         execute(dbOutput, "BEGIN TRANSACTION")
-        execute(dbOutput, "INSERT INTO hill_no2 VALUES (?,?,?)", (time,vhill2,bhill2))
+        execute(dbOutput, "INSERT INTO shannon_diversity VALUES (?,?,?)", (time,vshannon,bshannon))
         execute(dbOutput, "COMMIT")
 
-        if vtotal == 0 && btotal >= CARRYING_CAP
-            println("Virus has gone extinct! Also, microbes are at carrying capacity. Truncating data and projecting rest of computation...")
+        #if vtotal == 0 && btotal >= CARRYING_CAP
+            #println("Virus has gone extinct! Also, microbes are at carrying capacity. Truncating data and projecting rest of computation...")
 
-            timeData = [summary.t for summary in execute(dbTemp, "SELECT DISTINCT t FROM summary")]
-            timeDataTrunc = timeData[timeData.>time]
-            timesLeft = length(timeDataTrunc)
+            #timeData = [summary.t for summary in execute(dbTemp, "SELECT DISTINCT t FROM summary")]
+            #timeDataTrunc = timeData[timeData.>time]
+            #timesLeft = length(timeDataTrunc)
 
-            bsubAbunds = [bsubAbunds.abundance for bsubAbunds in execute(dbTemp, "SELECT abundance FROM babundance WHERE t = ?", (time,))]
-            brelAbunds = bsubAbunds./btotal
-            brelAbunds = brelAbunds[brelAbunds.>=1e-200]
+            #bsubAbunds = [bsubAbunds.abundance for bsubAbunds in execute(dbTemp, "SELECT abundance FROM babundance WHERE t = ?", (time,))]
+            #brelAbunds = bsubAbunds./btotal
+            #brelAbunds = brelAbunds[brelAbunds.>=1e-200]
 
             # This is assuming diversity will not reach +Inf
             #if true in isnan.(brelAbunds.*(log.(brelAbunds)))
-                #bhill2 = 0
+                #bshannon = 0
             #else
-                bhill2 = exp(-1*sum(brelAbunds.*(log.(brelAbunds))))
+                #bshannon = exp(-1*sum(brelAbunds.*(log.(brelAbunds))))
             #end
 
 
-            tempDF = DataFrame(t = Array{Float64,1}(timeDataTrunc),vhill2 = Array{Float64,1}(zeros(timesLeft)),bhill2 = Array{Float64,1}(bhill2*ones(timesLeft)))
-            tempDF |> SQLite.load!(dbOutput,"hill_no2",ifnotexists=true)
+            #tempDF = DataFrame(t = Array{Float64,1}(timeDataTrunc),vshannon = Array{Float64,1}(zeros(timesLeft)),bshannon = Array{Float64,1}(bshannon*ones(timesLeft)))
+            #tempDF |> SQLite.load!(dbOutput,"shannon_diversity",ifnotexists=true)
 
-            return
-        end
+            #return
+        #end
     end
 end
 
 shannon(dbTemp,dbOutput)
-
-tableNamesTypes = ["$(table_info.name) $(table_info.type)" for table_info in execute(dbSimInfo,"PRAGMA table_info(param_combos)")]
-tableNamesTypes = join(tableNamesTypes,", ")
-
-execute(dbOutput, "CREATE TABLE runs (run_id INTEGER, combo_id INTEGER, replicate INTEGER)")
-execute(dbOutput, "CREATE TABLE param_combos ($(tableNamesTypes...))")
-
-tableNames = ["$(table_info.name)" for table_info in execute(dbSimInfo,"PRAGMA table_info(param_combos)")]
-tableNames = join(tableNames,", ")
-execute(dbOutput, "BEGIN TRANSACTION")
-execute(dbOutput,"ATTACH DATABASE '$(dbSimInfoPath)' as dbSimInfo")
-execute(dbOutput,"INSERT INTO param_combos($(tableNames)) SELECT * FROM dbSimInfo.param_combos")
-execute(dbOutput,"INSERT INTO runs (run_id, combo_id, replicate) SELECT run_id, combo_id, replicate FROM dbSimInfo.runs")
-execute(dbOutput, "COMMIT")
 
 println("Complete!")
