0347688127732e7cab6c09456c97f9cd99509509
rliag11 <liaghat.armun@gmail.com>
2022-01-17 15:59:01 -0600

    Some aesthetic tweaks.

--- simulation/src/make-plots.py
+++ simulation/src/make-plots.py
@@ -10,6 +10,7 @@ import os
 import seaborn as sns
 from scipy import stats
 import sqlite3
+import matplotlib.ticker as ticker
 
 run_id = sys.argv[1]
 
@@ -33,25 +34,36 @@ print('SQLite Query: microbe abundance data')
 microbe = pd.read_sql_query("SELECT t,bstrain_id,abundance FROM babundance WHERE run_id = {}".format(run_id), con)
 microbe_stacked = microbe.pivot(index='t',columns='bstrain_id',values='abundance')
 
-pal = sns.color_palette("tab20b")
-
-print('Compiling microbial strain time series plot')
-microbe_stacked.plot.area(stacked=True, legend=False, linewidth=0,color=pal)
-plt.title('Microbial Strain Abundances (run{0}-c{1}-r{2})'.format(run_id,combo_id,replicate))
-plt.xlabel('Time t')
-plt.ylabel('Abundances N_i')
-plt.tight_layout()
-plt.savefig(os.path.join(PLOT_PATH,'microbe-strain-abundances.png'),dpi=500)
-
 print('SQLite Query: virus abundance data')
 virus = pd.read_sql_query("SELECT t,vstrain_id,abundance FROM vabundance WHERE run_id = {}".format(run_id), con)
 virus_stacked = virus.pivot(index='t',columns='vstrain_id',values='abundance')
 
+if microbe['t'][microbe['t'].size-1] not in virus_stacked.index:
+    microbe_stacked.drop(microbe['t'][microbe['t'].size-1],inplace=True)
+
+pal = sns.color_palette("tab20b")
+
+print('Compiling microbial strain time series plot')
+fig, ax = plt.subplots(1)
+microbe_stacked.plot.area(ax=ax, stacked=True, legend=False, linewidth=0,color=pal)
+ax.set_ylabel(ylabel ='Microbial Strain Abundances',labelpad=15,fontsize=7)
+ax.set_xlabel(xlabel = 'Time t',fontsize=7)
+ax.ticklabel_format(axis = 'y',style='sci',scilimits=(0,0))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(50))
+lim = ax.get_ylim()
+ax.set_ylim(0,lim[1])
+plt.tight_layout()
+plt.savefig(os.path.join(PLOT_PATH,'microbe-strain-abundances.png'),dpi=500)
+
 print('Compiling viral strain time series plot')
-virus_stacked.plot.area(stacked=True, legend=False, linewidth=0,color=pal)
-plt.title('Viral Strain Abundances (run{0}-c{1}-r{2})'.format(run_id,combo_id,replicate))
-plt.xlabel('Time t')
-plt.ylabel('Abundances V_i')
+fig, ax = plt.subplots(1)
+virus_stacked.plot.area(ax=ax, stacked=True, legend=False, linewidth=0,color=pal)
+ax.set_ylabel(ylabel ='Viral Strain Abundances',labelpad=15,fontsize=7)
+ax.set_xlabel(xlabel = 'Time t',fontsize=7)
+ax.ticklabel_format(axis = 'y',style='sci',scilimits=(0,0))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(50))
+lim = ax.get_ylim()
+ax.set_ylim(0,lim[1])
 plt.tight_layout()
 plt.savefig(os.path.join(PLOT_PATH,'virus-strain-abundances.png'),dpi=500)
 
@@ -63,13 +75,17 @@ axes = [ax[0], ax[1]]
 microbe_stacked.plot.area(ax = axes[0],stacked=True,legend=False, linewidth=0,color=pal)
 axes[0].set_ylabel(ylabel ='Microbial Immune Abundances N_i',labelpad=15,fontsize=7)
 axes[0].set_xlabel(xlabel = 'Time t',fontsize=7)
-axes[0].ticklabel_format(style='sci',scilimits=(0,0))
+axes[0].ticklabel_format(axis = 'y',style='sci',scilimits=(0,0))
+axes[0].xaxis.set_minor_locator(ticker.MultipleLocator(50))
 lim = axes[0].get_ylim()
 axes[0].set_ylim(0,lim[1])
 virus_stacked.plot.area(ax = axes[1],stacked=True,legend=False, linewidth=0,color=pal)
 axes[1].set_ylabel(ylabel ='Viral Strain Abundances V_i',labelpad=15,fontsize=7)
 axes[1].set_xlabel(xlabel = 'Time t',fontsize=7)
-axes[1].ticklabel_format(style='sci',scilimits=(0,0))
+axes[1].ticklabel_format(axis = 'y',style='sci',scilimits=(0,0))
+axes[1].xaxis.set_minor_locator(ticker.MultipleLocator(50))
+lim = axes[1].get_ylim()
+axes[1].set_ylim(0,lim[1])
 
 fig.tight_layout()
 fig.savefig(os.path.join(PLOT_PATH,'microbe-virus-stacked-abundances.png'),dpi=500)
