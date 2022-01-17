938b1788f11495cab3c2e35f765264319fdeaf37
rliag11 <liaghat.armun@gmail.com>
2022-01-17 15:55:26 -0600

    color and axes adjustments

--- data-analysis/clade-abundances/make-clade-abundances-plots.py
+++ data-analysis/clade-abundances/make-clade-abundances-plots.py
@@ -10,11 +10,13 @@ import os
 import seaborn as sns
 from scipy import stats
 import sqlite3
-from scipy.interpolate import griddata
-from scipy.interpolate import interp1d
+# from scipy.interpolate import griddata
+# from scipy.interpolate import interp1d
 import matplotlib.ticker as ticker
 import matplotlib.cm as cm
 from matplotlib.colors import Normalize
+import matplotlib.colors as mc
+import colorsys
 
 run_id = sys.argv[1]
 
@@ -31,11 +33,37 @@ DBCLADE_PATH = os.path.join(SCRIPT_PATH,'..','gathered-analyses','clade-abundanc
 #DBCLADE_PATH = os.path.join('/Volumes','Yadgah','clade-abundances.sqlite') # local
 # DBCLADE_PATH = os.path.join('/Volumes','Yadgah','clade-abundances_output.sqlite') # local. run_id fixed; for testing
 
+
+# def adjust_lightness(color, amount):
+#     try:
+#         c = mc.cnames[color]
+#     except:
+#         c = color
+#     c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+#     return colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2])
+
+def adjust_first_lightness(color, amountLight, amountSat):
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], amountLight, amountSat)
+
+def adjust_lightness(color, amountLight,amountSat):
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], amountLight*c[1], amountSat*c[2])
+
 conSim = sqlite3.connect(DBSIM_PATH)
 curSim = conSim.cursor()
 print('SQLite Query: microbial abundance time series data')
 microbeSim = pd.read_sql_query("SELECT t,bstrain_id,abundance FROM babundance WHERE run_id = {}".format(run_id), conSim)
 microbe_stacked = microbeSim.pivot(index='t',columns='bstrain_id',values='abundance')
+microbe_stacked.drop(2000.0,inplace=True)
 print('SQLite Query: viral abundance time series data')
 virusSim = pd.read_sql_query("SELECT t,vstrain_id,abundance FROM vabundance WHERE run_id = {}".format(run_id), conSim)
 virus_stacked = virusSim.pivot(index='t',columns='vstrain_id',values='abundance')
@@ -103,10 +131,10 @@ microbeColorDict = {}
 cladeColorDict = {}
 cladeDict = {}
 colorInd = 0
-for id in microbeCladeIDs.values:
+for clade_id in microbeCladeIDs.values:
     # print(id[0])
-    cladeColorDict[id[0]] = colorInd
-    cladeDict[id[0]] = []
+    cladeColorDict[clade_id[0]] = colorInd
+    cladeDict[clade_id[0]] = []
     colorInd += 1
 
 for strain in microbe_stacked.columns.values:
@@ -114,8 +142,8 @@ for strain in microbe_stacked.columns.values:
     cladeDict[clade] = np.append(cladeDict[clade],strain)
 
 columnOrder = []
-for id in microbeCladeIDs.values:
-    columnOrder = np.append(columnOrder,cladeDict[id[0]])
+for clade_id in microbeCladeIDs.values:
+    columnOrder = np.append(columnOrder,cladeDict[clade_id[0]])
 
 columnOrder = columnOrder.astype(int)
 microbe_stacked = microbe_stacked[columnOrder]
@@ -124,16 +152,32 @@ for strain in microbe_stacked.columns.values:
     clade = microbeClades[microbeClades['bstrain_id']==strain]['clade_id'].values[0]
     microbeColorDict[strain] = Mcmap(Mnorm(np.arange(1, len(microbeCladeIDs)+1, 1)))[cladeColorDict[clade]]
 
+# # for clade_id in microbeCladeIDs.values:
+# #     # print(id[0])
+# #     shadeColorDict[int(clade_id[0])] = Mcmap(Mnorm(np.arange(1, len(microbeCladeIDs)+1, 1)))[cladeColorDict[int(clade_id)]]
+# shadeColorDict = {}
+# for clade_id in microbeCladeIDs.values:
+#     # print(id[0])
+#     shadeColorDict[int(clade_id[0])] = \
+#     adjust_first_lightness(Mcmap(Mnorm(np.arange(1, len(microbeCladeIDs)+1, 1)))[cladeColorDict[int(clade_id)]],.15,1)
+#
+# for strain in microbe_stacked.columns.values:
+#     clade = microbeClades[microbeClades['bstrain_id']==strain]['clade_id'].values[0]
+#     microbeColorDict[strain] = shadeColorDict[clade]
+#     shadeColorDict[clade] = adjust_lightness(shadeColorDict[clade], 1.08,.9)
+
 fig, ax = plt.subplots(1)
 fig.suptitle('(run{0}-c{1}-r{2})'.format(run_id,combo_id,replicate))
 microbe_stacked.plot.area(ax = ax,stacked=True,legend=False, linewidth=0,color=microbeColorDict,sort_columns=True)
-microbe_stacked.plot(stacked=True, ax=ax, legend=False, color='black',sort_columns=True,linewidth=.1)
+microbe_stacked.plot(stacked=True, ax=ax, legend=False, color='white',sort_columns=True,linewidth=.3)
 ax.set_ylabel(ylabel ='Microbial Strain Abundances',labelpad=15,fontsize=7)
 ax.set_xlabel(xlabel = 'Time t',fontsize=7)
-ax.ticklabel_format(style='sci',scilimits=(0,0))
+ax.ticklabel_format(axis = 'y',style='sci',scilimits=(0,0))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(50))
 lim = ax.get_ylim()
 ax.set_ylim(0,lim[1])
 fig.tight_layout()
+# plt.show()
 fig.savefig(os.path.join(PLOT_PATH,'microbe-clade-abundances.png'),dpi=500)
 # plt.show()
 
@@ -145,6 +189,7 @@ Vcmap = cm.get_cmap('turbo')
 Vnorm = Normalize(vmin=1, vmax=len(virusCladeIDs))
 virusColorDict = {}
 cladeColorDict = {}
+shadeColorDict = {}
 cladeDict = {}
 colorInd = 0
 for id in virusCladeIDs.values:
@@ -175,7 +220,8 @@ virus_stacked.plot.area(ax = ax,stacked=True,legend=False, linewidth=0,color=vir
 virus_stacked.plot(stacked=True, ax=ax, legend=False, color='black',sort_columns=True,linewidth=.2)
 ax.set_ylabel(ylabel ='Viral Strain Abundances',labelpad=15,fontsize=7)
 ax.set_xlabel(xlabel = 'Time t',fontsize=7)
-ax.ticklabel_format(style='sci',scilimits=(0,0))
+ax.ticklabel_format(axis = 'y',style='sci',scilimits=(0,0))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(50))
 lim = ax.get_ylim()
 ax.set_ylim(0,lim[1])
 fig.tight_layout()
@@ -191,14 +237,16 @@ microbe_stacked.plot.area(ax = axes[0],stacked=True,legend=False, linewidth=0,co
 microbe_stacked.plot(stacked=True, ax=axes[0], legend=False, color='black',sort_columns=True,linewidth=.1)
 axes[0].set_ylabel(ylabel ='Microbial Strain Abundances',labelpad=15,fontsize=7)
 axes[0].set_xlabel(xlabel = 'Time t',fontsize=7)
-axes[0].ticklabel_format(style='sci',scilimits=(0,0))
+axes[0].ticklabel_format(axis = 'y',style='sci',scilimits=(0,0))
+axes[0].xaxis.set_minor_locator(ticker.MultipleLocator(50))
 lim = axes[0].get_ylim()
 axes[0].set_ylim(0,lim[1])
 virus_stacked.plot.area(ax = axes[1],stacked=True,legend=False, linewidth=0,color=virusColorDict,sort_columns=True)
 virus_stacked.plot(stacked=True, ax=axes[1], legend=False, color='black',sort_columns=True,linewidth=.1)
 axes[1].set_ylabel(ylabel ='Viral Strain Abundances',labelpad=15,fontsize=7)
 axes[1].set_xlabel(xlabel = 'Time t',fontsize=7)
-axes[1].ticklabel_format(style='sci',scilimits=(0,0))
+axes[1].ticklabel_format(axis = 'y',style='sci',scilimits=(0,0))
+axes[1].xaxis.set_minor_locator(ticker.MultipleLocator(50))
 lim = axes[1].get_ylim()
 axes[1].set_ylim(0,lim[1])
 fig.tight_layout()
